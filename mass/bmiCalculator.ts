aec3d65a9869c18fa9b2d26010d29ff5ed200298
skvoren <zayonvx@gmail.com>
2022-11-27 11:30:26 +0400

    exercise 9.1-9.3

---
+++ mass/bmiCalculator.ts
@@ -0,0 +1,9 @@
+const bmiCalculator = (height: number, weight: number) => {
+    console.log("Normal (Healthy weight)")
+}
+
+const height: number = Number(process.argv[2])
+const weight: number = Number(process.argv[3])
+
+bmiCalculator(height, weight)
+
